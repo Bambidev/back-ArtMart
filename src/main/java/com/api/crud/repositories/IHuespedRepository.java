835687ce39f71a68522d6f3c348fd80f47c5cfcf
Nico <orietasebastian@hotmail.com>
2024-04-09 00:10:58 -0300

    Delete Huesped, Add propertis

--- src/main/java/com/api/crud/repositories/IHuespedRepository.java
+++
@@ -1,6 +0,0 @@
-package com.api.crud.repositories;
-
-import com.api.crud.models.Huesped;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IHuespedRepository extends JpaRepository<Huesped, Long> { }
--- src/main/java/com/api/crud/repositories/IReservaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.api.crud.repositories;
-
-import com.api.crud.models.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IReservaRepository extends JpaRepository<Reserva, Long> { }
