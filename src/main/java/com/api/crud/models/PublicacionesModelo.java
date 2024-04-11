e5ab96e9d8b1e1c5854895da4cb23fc2ee5e1431
Dwith Gregory Newball De Avila <desarrollador.web@e-tic.co>
2024-04-11 16:28:41 -0500

    Create Publicaciones

---
+++ src/main/java/com/api/crud/models/PublicacionesModelo.java
@@ -0,0 +1,30 @@
+
+package com.api.crud.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "publicaciones")
+
+public class PublicacionesModelo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idpublicaciones")
+    private Integer idPublicaciones;
+    
+    @Column(name="titulo")
+    private String titulo;
+    
+    @Column(name="precio")
+    private double precio;
+    
+    @Column(name="descripcion")
+    private String descripcion;
+    
+    @Column(name="urlImagen")
+    private String urlImagen;
+            
+    
+}
