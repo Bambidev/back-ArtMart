7599648315845bac735f534a51e690021dae6bac
Nico <orietasebastian@hotmail.com>
2024-04-25 23:21:04 -0300

    update cors2

--- src/main/java/com/api/crud/CrudApplication.java
+++ src/main/java/com/api/crud/CrudApplication.java
@@ -18,7 +18,7 @@ public class CrudApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("*").allowedHeaders("*");
+				registry.addMapping("/api").allowedOrigins("http://localhost:5173/").allowedMethods("*").allowedHeaders("*");
 			}
 		};
 	}
